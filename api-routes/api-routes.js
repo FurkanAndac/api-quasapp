a521ce0dae6e171012843ee6f3e6180be43e3c32
FurkanAndac <furkan_andac_@hotmail.com>
2021-01-08 03:05:08 +0100

    Add getter for all vacancies with a pagelimit of 2, sort of testing pagination all the while implementing a vacancy API

--- api-routes/api-routes.js
+++ api-routes/api-routes.js
@@ -28,33 +28,30 @@ router.route('/users')
     .get(userController.getUsers)
 router.route('/users/:user_id')
     .get(userController.getUser)
-
 router.route('/users/:user_id/username')
     .get(userController.getUserName)
     .put(userController.updateUserName)
-
 router.route('/users/:user_id/surname')
     .get(userController.getSurname)
     .put(userController.updateSurname)
-
 router.route('/users/:user_id/gender')
     .get(userController.getGender)
     .put(userController.updateGender)
-
 router.route('/users/:user_id/email')
-
     .get(userController.getEmail)
     .put(userController.updateEmail)
-
 router.route('/users/:user_id/phone')
     .get(userController.getPhone)
     .put(userController.updatePhone)
-    
 router.route('/users/:user_id/cv')
     .get(userController.getCV)
     .put(userController.updateCV)
 
-
+//Import vacancy controller
+var vacancyController = require('../controller/vacancyController')
+// Vacancy routes
+router.route('/vacancies?limit=2')
+    .get(vacancyController.getVacancies)
 
 // Export API routes
 module.exports = router;
---
+++ model/vacancyModel.js
@@ -0,0 +1,37 @@
+// vacancyModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var vacancySchema = mongoose.Schema({
+    company: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    contact: {
+        phone: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        }
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    },  
+
+});
+// Export Vacancy model
+var Vacancy = module.exports = mongoose.model('user', vacancySchema);
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
