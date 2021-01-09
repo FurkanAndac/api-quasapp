0cb276a1ef2ae8ef012d301e148c9f7b67dfc59e
FurkanAndac <furkan_andac_@hotmail.com>
2021-01-09 21:26:52 +0100

    amk

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(cors());
 
 // Enable all origins
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   req.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Origin', 'https://quasapp-95a6c.web.app');
    next();
  });
 
